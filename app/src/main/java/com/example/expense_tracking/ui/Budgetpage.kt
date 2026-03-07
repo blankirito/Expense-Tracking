c6bf284653f536ba75771b46695320fb2972fc27
Lee Chong Yu <leechongyu99@gmail.com>
2026-03-07 19:12:06 +0800

    disable delete budget function

--- app/src/main/java/com/example/expense_tracking/ui/Budgetpage.kt
+++ app/src/main/java/com/example/expense_tracking/ui/Budgetpage.kt
@@ -498,7 +498,7 @@ fun Budgetpage_CategoryBudgetItem(
             },
             dismissButton = {
                 Row {
-                    Button(
+                    /*Button(
                         onClick = {
                             onDeleteCategory(category)
                             showDialog = false
@@ -506,7 +506,7 @@ fun Budgetpage_CategoryBudgetItem(
                         colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
                     ) {
                         Text("Delete", color = Color.White)
-                    }
+                    }*/
 
                     Spacer(modifier = Modifier.width(8.dp))
 
